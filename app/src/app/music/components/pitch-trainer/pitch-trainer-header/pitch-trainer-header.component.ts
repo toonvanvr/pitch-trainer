7aead2adcdcf8d1ab4d6dd29c56e51204b9f26da
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2024-06-11 23:46:41 +0200

    feat: organize UI a little

--- app/src/app/music/components/pitch-trainer/pitch-trainer-header/pitch-trainer-header.component.ts
+++ app/src/app/music/components/pitch-trainer/pitch-trainer-header/pitch-trainer-header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
 @Component({
   selector: 'app-pitch-trainer-header',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './pitch-trainer-header.component.html',
   styleUrl: './pitch-trainer-header.component.scss',
 })
