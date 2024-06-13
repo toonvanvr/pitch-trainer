b31e4674ce69e658c8d7b6c23f9f9edaa8d7aeef
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2024-06-13 15:34:45 +0200

    feat: expand navigation depending on section

--- app/src/app/music/components/navigation/navigation.component.ts
+++ app/src/app/music/components/navigation/navigation.component.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatExpansionModule } from '@angular/material/expansion'
-import { RouterModule } from '@angular/router'
+import { ActivatedRoute, RouterModule } from '@angular/router'
+import { map } from 'rxjs'
 import { MusicSchoolAuthService } from '../../services/music-school-auth.service'
 import { PitchTrainerConfigComponent } from '../pitch-trainer/pitch-trainer-config/pitch-trainer-config.component'
 
@@ -20,7 +21,15 @@ import { PitchTrainerConfigComponent } from '../pitch-trainer/pitch-trainer-conf
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
-  constructor(private readonly musicSchoolAuth: MusicSchoolAuthService) {}
+  constructor(
+    private readonly musicSchoolAuth: MusicSchoolAuthService,
+    private readonly route: ActivatedRoute,
+  ) {}
+  readonly examType$ = this.route.url.pipe(
+    map((url) => {
+      return url[1].path.split('-')[1]
+    }),
+  )
 
   isAuthenticated$ = this.musicSchoolAuth.isAuthenticated$
 }
